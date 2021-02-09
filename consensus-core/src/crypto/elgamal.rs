06fce1aa32206fe37c0ca42b6e249b03350e2a09
Henrik Knudsen <henrik@knudsenhome.no>
2021-02-09 21:51:49 +0100

    Filled out all functionality of threshold cryptosystem

---
+++ consensus-core/src/crypto/elgamal.rs
@@ -0,0 +1,132 @@
+struct ElgamalCipherPair {
+    c1: ProjectivePoint,
+    c2: ProjectivePoint,
+}
+
+/// Encode message into vector of Scalars
+fn encode(msg: &str) -> Vec<Scalar> {
+    let mut bytes = msg.as_bytes();
+
+    let mut scalars = vec![];
+
+    while let Ok(num) = bytes.read_u64::<BigEndian>() {
+        let scalar = Scalar::from(num);
+        scalars.push(scalar);
+    }
+
+    // Handle remaining bytes, if any
+    if bytes.len() > 0 {
+        let padding = vec![0; 8 - bytes.len()];
+        let num = [padding.as_slice(), bytes]
+            .concat()
+            .as_slice()
+            .read_u64::<BigEndian>()
+            .unwrap();
+
+        let scalar = p256::Scalar::from(num);
+        scalars.push(scalar);
+    }
+
+    scalars
+}
+
+fn encode_to_points(msg: &str) -> Vec<EncodedPoint> {
+    vec![]
+}
+
+fn decode(scalars: Vec<Scalar>) -> Vec<u8> {
+    scalars
+        .into_iter()
+        .map(|scalar| scalar.to_bytes().as_slice().to_owned())
+        .flatten()
+        .collect()
+}
+
+fn elgamal(msg: &str, public: &PublicKey) -> Vec<u8> {
+    let scalars = encode(msg);
+
+    let y = RngCore::next_u64(&mut OsRng);
+
+    let y = Scalar::from(y);
+
+    decode(scalars)
+}
+
+#[cfg(test)]
+mod tests {
+
+    use std::{
+        borrow::Borrow,
+        ops::{Deref, Mul},
+    };
+
+    use super::*;
+
+    #[test]
+    fn test_gen_key() {
+        let (public, private) = generate_keys().unwrap();
+    }
+
+    #[test]
+    fn test_elgamal() {
+        let (public, private) = generate_keys().unwrap();
+
+        // public contains: random point alpha, point beta (from beta = alpha * a) + Curve and modulus p.
+
+        let generator = ProjectivePoint::generator();
+
+        // private contains secret integer, less than p.
+        let secret_scalar = private.secret_scalar();
+
+        let msg = "Hello world everyone here!";
+        println!("Message: {}", msg);
+
+        let cipher = elgamal(msg, &public);
+
+        let resp = str::from_utf8(cipher.as_slice()).unwrap();
+
+        println!("Response: {}", resp);
+    }
+
+    #[test]
+    fn test_ecdh() {
+        // Alice
+        let alice_secret = EphemeralSecret::random(&mut OsRng);
+        let alice_pk_bytes = EncodedPoint::from(alice_secret.public_key());
+
+        // Bob
+        let bob_secret = EphemeralSecret::random(&mut OsRng);
+        let bob_pk_bytes = EncodedPoint::from(bob_secret.public_key());
+
+        // Alice decodes Bob's serialized public key and computes a shared secret from it
+        let bob_public = PublicKey::from_sec1_bytes(bob_pk_bytes.as_ref())
+            .expect("bob's public key is invalid!"); // In real usage, don't panic, handle this!
+
+        let alice_shared = alice_secret.diffie_hellman(&bob_public);
+
+        // Bob deocdes Alice's serialized public key and computes the same shared secret
+        let alice_public = PublicKey::from_sec1_bytes(alice_pk_bytes.as_ref())
+            .expect("alice's public key is invalid!"); // In real usage, don't panic, handle this!
+
+        let bob_shared = bob_secret.diffie_hellman(&alice_public);
+
+        // Both participants arrive on the same shared secret
+        assert_eq!(alice_shared.as_bytes(), bob_shared.as_bytes());
+    }
+
+    #[test]
+    fn test_elliptic_curve_mul() {
+        let (public, private) = generate_keys().unwrap();
+
+        let generator = ProjectivePoint::generator();
+
+        // private contains secret integer, less than p.
+        let secret_scalar = private.secret_scalar();
+
+        let result = generator.mul(secret_scalar.deref());
+
+        // point kept inside public key should be g * secret
+
+        assert_eq!(public.to_projective(), result)
+    }
+}
--- consensus-core/src/crypto/threshold.rs
+++ consensus-core/src/crypto/threshold.rs
@@ -7,14 +7,15 @@ use openssl::{
 use byteorder::{BigEndian, ReadBytesExt};
 use p256::*;
 use p256::{
-    ecdh::EphemeralSecret, elliptic_curve::Group, EncodedPoint, NistP256, PublicKey, SecretKey,
+    ecdh::EphemeralSecret, elliptic_curve::Group, EncodedPoint, NistP256, ProjectivePoint,
+    PublicKey, SecretKey,
 };
 use p256::{elliptic_curve::ff::PrimeField, pkcs8::FromPrivateKey};
 use p256::{pkcs8::FromPublicKey, Scalar};
 use rand_core::{CryptoRng, OsRng, RngCore};
-use std::{error::Error, io::Read, ops::Deref, str};
+use std::{collections::HashSet, error::Error, io::Read, ops::Deref, str};
 
-use super::hash::hash_sha256;
+use super::hash::{hash1, hash2, hash4, hash_sha256};
 
 const CURVE_ID: Nid = Nid::X9_62_PRIME256V1;
 /// TODO: Remove this stuff, not needed. Can generate keys through p256 only.
@@ -50,7 +51,7 @@ impl Dealer {
             .map(|_| NonZeroScalar::random(&mut OsRng))
             .collect();
 
-        let secrets: Vec<Scalar> = (0..n_actors)
+        let secrets: Vec<Scalar> = (0..n_actors + 1)
             .map(|i| {
                 let mut sum = Scalar::zero();
                 let mut factor = Scalar::one();
@@ -94,180 +95,416 @@ pub struct KeySet {
     secrets: Vec<SecretK>,
 }
 
+#[derive(Clone, Copy)]
 struct PublicK {
     g: AffinePoint,
     g1: AffinePoint,
     h: AffinePoint,
 }
-
+#[derive(Clone)]
 struct VerifyK {
     elements: Vec<AffinePoint>,
 }
-
+#[derive(Clone, Copy)]
 struct SecretK {
     secret_scalar: Scalar,
 }
 
 /// Actor within the threshold cryptosystem. Has access to public key, verification key, and their own secret key.
 pub struct Actor {
+    index: usize,
     public: PublicK,
     verify: VerifyK,
     secret: SecretK,
 }
 
 impl Actor {
-    pub fn encrypt(data: &[u8]) {
-        let r = AffinePoint::generator() * NonZeroScalar::random(&mut OsRng);
-        let s = AffinePoint::generator() * NonZeroScalar::random(&mut OsRng);
+    pub fn encrypt(&self, data: &[u8; 32], label: &[u8; 32]) -> Ciphertext {
+        let r = NonZeroScalar::random(&mut OsRng);
+        let s = NonZeroScalar::random(&mut OsRng);
 
-        todo!()
+        let h1 = hash1(*&self.public.h * r);
+        let c: Vec<u8> = data.iter().zip(&h1).map(|(m, h)| m ^ h).collect();
+
+        let u = *&self.public.g * r;
+        let w = *&self.public.g * s;
+        let u1 = *&self.public.g1 * r;
+        let w1 = *&self.public.g1 * s;
+
+        let e = hash2(&c, label, u, w, u1, w1);
+        let f = s.deref() + &(r.deref() * &e);
+
+        Ciphertext {
+            c,
+            label: label.to_vec(),
+            u,
+            u1,
+            e,
+            f,
+        }
     }
-    pub fn extract_label(ciphertext: &[u8]) {
-        todo!()
+    pub fn extract_label(&self, ciphertext: &Ciphertext) -> Vec<u8> {
+        ciphertext.label.clone()
     }
 
-    pub fn decrypt_share(ciphetext: &[u8]) {
-        todo!()
+    pub fn decrypt_share(&self, ciphertext: &Ciphertext) -> Option<DecryptionShare> {
+        if !self.is_proof_valid(&ciphertext) {
+            return None;
+        };
+
+        let ss = NonZeroScalar::random(&mut OsRng);
+
+        let uu = (ProjectivePoint::from(ciphertext.u) * self.secret.secret_scalar).to_affine();
+        let uu1 = ciphertext.u * ss;
+        let hh = *&self.public.g * ss;
+        let ee = hash4(uu, uu1, hh);
+        let ff = *ss + (self.secret.secret_scalar * ee);
+
+        Some(DecryptionShare {
+            index: self.index,
+            uu,
+            ee,
+            ff,
+        })
+    }
+    // TODO: Verify_share should really accept a decryption share for a invalid ciphertext (e.g. "i, ?"). Currently just return false on invalid proof.
+    pub fn verify_share(&self, ciphertext: &Ciphertext, decrypt_share: &DecryptionShare) -> bool {
+        if !self.is_proof_valid(&ciphertext) {
+            return false;
+        };
+
+        let DecryptionShare { uu, ee, ff, index } = decrypt_share;
+
+        assert!(
+            &self.verify.elements.len() > index,
+            "Fewer verify elements ({}) than index {}",
+            &self.verify.elements.len(),
+            index
+        );
+
+        let uu1 = (ProjectivePoint::from(ciphertext.u) * ff) - (ProjectivePoint::from(*uu) * ee);
+        let hh1 = (ProjectivePoint::from(*&self.public.g) * ff)
+            - (ProjectivePoint::from(*&self.verify.elements[*index]) * ee);
+
+        *ee == hash4(*uu, uu1.to_affine(), hh1.to_affine())
     }
 
-    pub fn verify_share(decrypt_share: &[u8]) -> bool {
-        todo!()
+    pub fn combine_shares(
+        &self,
+        ciphertext: &Ciphertext,
+        shares: Vec<DecryptionShare>,
+    ) -> Option<Plaintext> {
+        if !self.is_proof_valid(&ciphertext) {
+            return None;
+        };
+
+        let coefficients = self.calculate_lagrange_coefficients(&shares);
+
+        // res = Summation over uu * coeff
+
+        let result = shares
+            .iter()
+            .zip(coefficients)
+            .fold(ProjectivePoint::identity(), |acc, (share, coeff)| {
+                acc + (ProjectivePoint::from(share.uu) * coeff)
+            });
+
+        // m = H1(res) XOR c
+        let h1 = hash1(result.to_affine());
+
+        let mut data = [0u8; 32];
+
+        data.copy_from_slice(
+            &h1.iter()
+                .zip(&ciphertext.c)
+                .map(|(h_ele, c_ele)| h_ele ^ c_ele)
+                .collect::<Vec<u8>>(),
+        );
+
+        Some(Plaintext { data })
     }
 
-    pub fn combine_shares(shares: &[u8]) {
-        todo!()
+    fn is_proof_valid(&self, ciphertext: &Ciphertext) -> bool {
+        let Ciphertext {
+            c,
+            label,
+            u,
+            u1,
+            e,
+            f,
+        } = ciphertext;
+        let w = (ProjectivePoint::from(*&self.public.g) * f) - (ProjectivePoint::from(*u) * e);
+        let w1 = (ProjectivePoint::from(*&self.public.g1) * f) - (ProjectivePoint::from(*u1) * e);
+        ciphertext.e == hash2(c, label, *u, w.to_affine(), *u1, w1.to_affine())
+    }
+
+    fn calculate_lagrange_coefficients(&self, shares: &Vec<DecryptionShare>) -> Vec<Scalar> {
+        let index_set: HashSet<_> = shares.iter().map(|share| share.index).collect();
+
+        // Interpolate over the points, get coefficients
+        shares
+            .iter()
+            .map(|share| {
+                // S = subset of Zq (Set of scalars). i = 0. j is the index of the share.
+
+                let numerator = index_set.iter().fold(Scalar::one(), |acc, index| {
+                    acc * match index {
+                        index if index == &share.index => Scalar::one(),
+                        _ => {
+                            Scalar::zero()
+                                - Scalar::one() * Scalar::from(*index as u64)
+                                - Scalar::one()
+                        }
+                    }
+                });
+
+                let denumerator = index_set.iter().fold(Scalar::one(), |acc, index| {
+                    acc * match index {
+                        index if index == &share.index => Scalar::one(),
+                        _ => {
+                            Scalar::one() * Scalar::from(share.index as u64)
+                                - Scalar::one() * Scalar::from(*index as u64)
+                        }
+                    }
+                });
+
+                assert_ne!(numerator, Scalar::zero(), "Expect numerator to not be zero");
+                assert_ne!(
+                    denumerator,
+                    Scalar::zero(),
+                    "Expect denumerator to not be zero"
+                );
+                // TODO: Ensure Denumerator is always  invertible, e.g. not zero.
+                numerator * denumerator.invert().unwrap()
+            })
+            .collect()
     }
 }
 
-struct ElgamalCipherPair {
-    c1: ProjectivePoint,
-    c2: ProjectivePoint,
+pub struct Ciphertext {
+    c: Vec<u8>,
+    label: Vec<u8>,
+    u: AffinePoint,
+    u1: AffinePoint,
+    e: Scalar,
+    f: Scalar,
 }
 
-/// Encode message into vector of Scalars
-fn encode(msg: &str) -> Vec<Scalar> {
-    let mut bytes = msg.as_bytes();
-
-    let mut scalars = vec![];
-
-    while let Ok(num) = bytes.read_u64::<BigEndian>() {
-        let scalar = Scalar::from(num);
-        scalars.push(scalar);
-    }
-
-    // Handle remaining bytes, if any
-    if bytes.len() > 0 {
-        let padding = vec![0; 8 - bytes.len()];
-        let num = [padding.as_slice(), bytes]
-            .concat()
-            .as_slice()
-            .read_u64::<BigEndian>()
-            .unwrap();
-
-        let scalar = p256::Scalar::from(num);
-        scalars.push(scalar);
-    }
-
-    scalars
+pub struct DecryptionShare {
+    index: usize,
+    uu: AffinePoint,
+    ee: Scalar,
+    ff: Scalar,
 }
 
-fn encode_to_points(msg: &str) -> Vec<EncodedPoint> {
-    vec![]
-}
-
-fn decode(scalars: Vec<Scalar>) -> Vec<u8> {
-    scalars
-        .into_iter()
-        .map(|scalar| scalar.to_bytes().as_slice().to_owned())
-        .flatten()
-        .collect()
-}
-
-fn elgamal(msg: &str, public: &PublicKey) -> Vec<u8> {
-    let scalars = encode(msg);
-
-    let y = RngCore::next_u64(&mut OsRng);
-
-    let y = Scalar::from(y);
-
-    decode(scalars)
+pub struct Plaintext {
+    data: [u8; 32],
 }
 
 #[cfg(test)]
 mod tests {
 
-    use std::{
-        borrow::Borrow,
-        ops::{Deref, Mul},
-    };
-
     use super::*;
 
+    const N_ACTORS_MULTIPLE: usize = 5;
+    const THRESHOLD_MULTIPLE: usize = 3;
+
     #[test]
-    fn test_gen_key() {
-        let (public, private) = generate_keys().unwrap();
+    fn test_proof_valid() {
+        let keyset = Dealer::generate_keys(N_ACTORS_MULTIPLE, THRESHOLD_MULTIPLE);
+
+        let actors: Vec<Actor> = (0..N_ACTORS_MULTIPLE)
+            .into_iter()
+            .map(|index| Actor {
+                index,
+                public: keyset.public,
+                verify: keyset.verify.clone(),
+                secret: keyset.secrets[index],
+            })
+            .collect();
+
+        let message = "Hello world! Hello world! Hello!";
+
+        let mut data = [0u8; 32];
+
+        data.copy_from_slice(message.as_bytes());
+
+        let encrypted = actors[0].encrypt(&data, &data);
+
+        for actor in &actors {
+            assert!(actor.is_proof_valid(&encrypted));
+        }
     }
 
     #[test]
-    fn test_elgamal() {
-        let (public, private) = generate_keys().unwrap();
+    fn test_verify_share() {
+        let keyset = Dealer::generate_keys(2, 1);
 
-        // public contains: random point alpha, point beta (from beta = alpha * a) + Curve and modulus p.
+        let actors: Vec<Actor> = (0..2)
+            .into_iter()
+            .map(|index| Actor {
+                index,
+                public: keyset.public,
+                verify: keyset.verify.clone(),
+                secret: keyset.secrets[index],
+            })
+            .collect();
 
-        let generator = ProjectivePoint::generator();
+        let message = "Hello world! Hello world! Hello!";
 
-        // private contains secret integer, less than p.
-        let secret_scalar = private.secret_scalar();
+        let mut data = [0u8; 32];
 
-        let msg = "Hello world everyone here!";
-        println!("Message: {}", msg);
+        data.copy_from_slice(message.as_bytes());
 
-        let cipher = elgamal(msg, &public);
+        let encrypted = actors[0].encrypt(&data, &data);
 
-        let resp = str::from_utf8(cipher.as_slice()).unwrap();
+        let decrypt_shares: Vec<_> = actors
+            .iter()
+            .map(|actor| actor.decrypt_share(&encrypted).unwrap())
+            .collect();
 
-        println!("Response: {}", resp);
+        assert_eq!(actors.len(), 2);
+        assert_eq!(decrypt_shares.len(), 2);
+        assert!(
+            actors[0].verify_share(&encrypted, &decrypt_shares[0]),
+            "Actor 0 could not verify share 0"
+        );
+        assert!(
+            actors[1].verify_share(&encrypted, &decrypt_shares[1]),
+            "Actor 1 could not verify share 1"
+        );
     }
 
     #[test]
-    fn test_ecdh() {
-        // Alice
-        let alice_secret = EphemeralSecret::random(&mut OsRng);
-        let alice_pk_bytes = EncodedPoint::from(alice_secret.public_key());
+    fn test_threshold_encrypt_single_actor() {
+        let keyset = Dealer::generate_keys(1, 1);
 
-        // Bob
-        let bob_secret = EphemeralSecret::random(&mut OsRng);
-        let bob_pk_bytes = EncodedPoint::from(bob_secret.public_key());
+        let actors: Vec<Actor> = (0..1)
+            .into_iter()
+            .map(|index| Actor {
+                index,
+                public: keyset.public,
+                verify: keyset.verify.clone(),
+                secret: keyset.secrets[index],
+            })
+            .collect();
 
-        // Alice decodes Bob's serialized public key and computes a shared secret from it
-        let bob_public = PublicKey::from_sec1_bytes(bob_pk_bytes.as_ref())
-            .expect("bob's public key is invalid!"); // In real usage, don't panic, handle this!
+        let message = "Hello world! Hello world! Hello!";
 
-        let alice_shared = alice_secret.diffie_hellman(&bob_public);
+        let mut data = [0u8; 32];
 
-        // Bob deocdes Alice's serialized public key and computes the same shared secret
-        let alice_public = PublicKey::from_sec1_bytes(alice_pk_bytes.as_ref())
-            .expect("alice's public key is invalid!"); // In real usage, don't panic, handle this!
+        data.copy_from_slice(message.as_bytes());
 
-        let bob_shared = bob_secret.diffie_hellman(&alice_public);
+        assert!(message.as_bytes().len() == 32);
 
-        // Both participants arrive on the same shared secret
-        assert_eq!(alice_shared.as_bytes(), bob_shared.as_bytes());
+        println!("Message bytes: {:?}", &message.as_bytes());
+        println!("Message bytes len: {:?}", &message.as_bytes().len());
+        println!(
+            "Message str: {:?}",
+            str::from_utf8(&message.as_bytes()).expect("Message data should be valid utf-8")
+        );
+
+        let encrypted = actors[0].encrypt(&data, &data);
+
+        println!("Ciphertext bytes: {:?}", &encrypted.c);
+        println!(
+            "Ciphertext label: {:?}",
+            str::from_utf8(&encrypted.label).expect("Label should be valid utf-8")
+        );
+
+        let decrypt_shares: Vec<_> = actors
+            .iter()
+            .map(|actor| actor.decrypt_share(&encrypted).unwrap())
+            .collect();
+
+        for share in &decrypt_shares {
+            for actor in &actors {
+                if actor.index != share.index {
+                    assert!(
+                        actor.verify_share(&encrypted, share),
+                        "Actor {} could not verify share: {}",
+                        actor.index,
+                        share.index
+                    );
+                }
+            }
+        }
+
+        let plaintext = actors[0]
+            .combine_shares(&encrypted, decrypt_shares)
+            .expect("Plaintext could not be retrieved from shares");
+
+        println!("Plaintext bytes: {:?}", &plaintext.data);
+        println!(
+            "Plaintext message: {:?}",
+            str::from_utf8(&plaintext.data).expect("Plaintext data should be valid utf-8")
+        );
     }
 
     #[test]
-    fn test_elliptic_curve_mul() {
-        let (public, private) = generate_keys().unwrap();
+    fn test_threshold_encrypt_multiple_actors() {
+        let keyset = Dealer::generate_keys(N_ACTORS_MULTIPLE, THRESHOLD_MULTIPLE);
 
-        let generator = ProjectivePoint::generator();
+        let actors: Vec<Actor> = (0..N_ACTORS_MULTIPLE)
+            .into_iter()
+            .map(|index| Actor {
+                index,
+                public: keyset.public,
+                verify: keyset.verify.clone(),
+                secret: keyset.secrets[index],
+            })
+            .collect();
 
-        // private contains secret integer, less than p.
-        let secret_scalar = private.secret_scalar();
+        let message = "Hello world! Hello world! Hello!";
 
-        let result = generator.mul(secret_scalar.deref());
+        let mut data = [0u8; 32];
 
-        // point kept inside public key should be g * secret
+        data.copy_from_slice(message.as_bytes());
 
-        assert_eq!(public.to_projective(), result)
+        assert!(message.as_bytes().len() == 32);
+
+        println!("Message bytes: {:?}", &message.as_bytes());
+        println!("Message bytes len: {:?}", &message.as_bytes().len());
+        println!(
+            "Message str: {:?}",
+            str::from_utf8(&message.as_bytes()).expect("Message data should be valid utf-8")
+        );
+
+        let encrypted = actors[0].encrypt(&data, &data);
+
+        println!("Ciphertext bytes: {:?}", &encrypted.c);
+        println!(
+            "Ciphertext label: {:?}",
+            str::from_utf8(&encrypted.label).expect("Label should be valid utf-8")
+        );
+
+        let decrypt_shares: Vec<_> = actors
+            .iter()
+            .map(|actor| actor.decrypt_share(&encrypted).unwrap())
+            .collect();
+
+        /*
+            for share in &decrypt_shares {
+            for actor in &actors {
+                if actor.index != share.index {
+                    assert!(
+                        actor.verify_share(&encrypted, share),
+                        "Actor {} could not verify share: {}",
+                        actor.index,
+                        share.index
+                    );
+                }
+            }
+        } */
+
+        let plaintext = actors[0]
+            .combine_shares(&encrypted, decrypt_shares)
+            .expect("Plaintext could not be retrieved from shares");
+
+        println!("Plaintext bytes: {:?}", &plaintext.data);
+        println!(
+            "Plaintext message: {:?}",
+            str::from_utf8(&plaintext.data).expect("Plaintext data should be valid utf-8")
+        );
     }
 }
