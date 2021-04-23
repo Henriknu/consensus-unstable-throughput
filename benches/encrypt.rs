5bedd8f7d4c350ca7ef9d3f7eabfece0bec2fc29
Henrik Knudsen <henrik@knudsenhome.no>
2021-04-23 16:57:29 +0200

    Extend bench for thresh encrypt to include pairing based from threshold_crypto

--- benches/encrypt.rs
+++ benches/encrypt.rs
@@ -1,19 +1,36 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 
+use threshold_crypto::{PublicKey, SecretKey};
+
 use consensus_core::crypto::encrypt::Encrypter;
 
-fn threshold_encrypt(encrypter: &Encrypter, data: &[u8; 32]) {
+fn threshold_encrypt_tdh2(encrypter: &Encrypter, data: &[u8; 32]) {
     encrypter.encrypt(data, data);
 }
 
-fn criterion_benchmark(c: &mut Criterion) {
+fn threshold_encrypt_pairing(encrypter: &PublicKey, data: &[u8; 32]) {
+    encrypter.encrypt(data);
+}
+
+fn bench_tdh2(c: &mut Criterion) {
     let encrypters = Encrypter::generate_keys(2, 1);
+    let encrypter = &encrypters[0];
+
     let data = [0u8; 32];
 
-    c.bench_function("threshold_encrypt", |b| {
-        b.iter(|| threshold_encrypt(&encrypters[0], &data))
+    c.bench_function("threshold_encrypt_tdh2", |b| {
+        b.iter(|| threshold_encrypt_tdh2(encrypter, &data))
     });
 }
 
-criterion_group!(benches, criterion_benchmark);
+fn bench_pairing(c: &mut Criterion) {
+    let public = SecretKey::random().public_key();
+    let data = [0u8; 32];
+
+    c.bench_function("threshold_encrypt_pairing", |b| {
+        b.iter(|| threshold_encrypt_pairing(&public, &data))
+    });
+}
+
+criterion_group!(benches, bench_tdh2, bench_pairing);
 criterion_main!(benches);
